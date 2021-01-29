178e70b958581b15a57868dd9fa75b162b429f22
PotatoWhite <bravopotato@gmail.com>
2021-01-29 17:53:41 +0900

    add swagger

--- demo-user-service/src/main/java/io/crcell/demo/Application.java
+++ demo-user-service/src/main/java/io/crcell/demo/Application.java
@@ -4,8 +4,9 @@ import io.crcell.simply.controllable.EnableSimplyControllable;
 import io.crcell.simply.eventable.producer.EnableSimplyProducer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-//@EnableSwagger2
+@EnableSwagger2
 @EnableSimplyProducer
 @EnableSimplyControllable
 @SpringBootApplication
